de6ea4a7a14a2af900186bb6b7aeae5cc320b56c
rajvipokiya <pokiya.rajvi51@gmail.com>
2025-04-10 09:32:05 -0400

    init commit

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,24 +10,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar:  AppBar(
         title: Center(child: Text("Rest Api Call", style: TextStyle(
             color: Colors.white),)),
         backgroundColor: Colors.cyan,
       ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          final imageUrl  =  user['picture']['thumbnail'];
+          final name =  user['name']['first'];
+          final email  =  user['email'];
+
+        return ListTile(
+        /*  leading: CircleAvatar(child: Text('${index +1 }'),),*/
+          leading: ClipRRect(borderRadius: BorderRadius.circular(100),
+            child:  Image.network(imageUrl,),
+          ),
+          title: Text(name),
+          subtitle: Text(email),
+        );
+
+      },),
       floatingActionButton: FloatingActionButton(
-          onPressed: fetchUser),
+          onPressed: fetchUser,),
     );
   }
 
-  void fetchUser(){
+  void fetchUser() async{
     print('fetchUser Called');
-    const url =""
+    const url ='https://randomuser.me/api/?results=10';
     final uri = Uri.parse(url);
-    http.get(uri);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json =  jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print('fetchUser completed');
   }
-
 }
+// Image.network(imageUrl,),
